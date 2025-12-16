615a895e5a12c4d8cf957cd93bf5790bcf019e8e
estebanEversoft <esteban.lopez@eversoft.mx>
2025-12-16 14:19:48 -0600

    Add: example tab route

---
+++ src/features/theme/screens/ThemeConfiguration.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ThemeConfiguration() {
+    return (
+        <View>
+            <Text>ThemeConfiguration</Text>
+        </View>
+    )
+}
--- src/navigation/navigators/BottomTabNavigator.tsx
+++ src/navigation/navigators/BottomTabNavigator.tsx
@@ -1,5 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { StackNavigator } from './StackNavigator';
+import ThemeConfiguration from '@src/features/theme/screens/ThemeConfiguration';
+import { ScreenLayout } from '../layouts/ScreenLayout';
 
 const Tab = createBottomTabNavigator();
 
@@ -7,9 +9,17 @@ export function BottomTabsNavigator() {
 
     return (
         <Tab.Navigator screenOptions={{
-            headerShown: false
+            headerShown: false,
+            animation: 'fade'
         }}>
             <Tab.Screen name="Home" component={StackNavigator} />
+            <Tab.Screen name="Theme" children={() => {
+                return (
+                    <ScreenLayout>
+                        <ThemeConfiguration />
+                    </ScreenLayout>
+                )
+            }} />
         </Tab.Navigator>
     );
 }
